94262e6a537ed203293f2cd467bf4ce04fe739f8
Praneeth <ambatisaipraneeth@gmail.com>
2017-02-01 15:21:55 -0600

    Implemented DateActivity with DatePickerDialogs

---
+++ app/src/main/java/com/example/praneethambati/differentbuttons/DateActivity.java
@@ -0,0 +1,50 @@
+package com.example.praneethambati.differentbuttons;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DateActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText dateET;
+    private SimpleDateFormat dateFormatter;
+    DatePickerDialog datePickerDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date);
+
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+
+        dateET = (EditText) findViewById(R.id.dateET);
+        dateET.setInputType(InputType.TYPE_NULL);
+        dateET.requestFocus();
+        dateET.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year,month,dayOfMonth);
+                dateET.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        }, newCalendar.get(Calendar.YEAR),newCalendar.get(Calendar.MONTH),newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == dateET) {
+            datePickerDialog.show();
+        }
+    }
+}
